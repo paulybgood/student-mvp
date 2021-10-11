50c26a221b4b4c382d19112ffb1b0987c67f1b54
Paul Bialas <paulybgood@gmail.com>
2021-10-11 15:44:13 -0500

    created migration and seed files for initial db info

---
+++ database-setup/migration.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users, todolist, tasks CASCADE;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT
+);
+
+CREATE TABLE todolist (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    user_id INTEGER REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    description TEXT,
+    todolist_id INTEGER REFERENCES todolist (id) ON DELETE CASCADE
+);
+
---
+++ database-setup/seed.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO users(username) VALUES('paulybgood');
+
+INSERT INTO todolist(name, user_id) VALUES('Student MVP', 1);
+
+INSERT INTO tasks(description, todolist_id) VALUES('Create the basic HTML', 1);
+INSERT INTO tasks(description, todolist_id) VALUES('Create the the serverside code to handle HTTP requests', 1);
